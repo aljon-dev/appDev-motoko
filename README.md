443db63227815ad44237c6f1bfbcfe81811b2b1c
Roland Bole <rbole@samlinux.at>
2024-11-01 10:54:51 +0100

    add Svelte5, SvelteKit and TailwindCSS

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# appDev-motoko
+# appDev - Motoko / Svelte 5 Starter
+
+<a href='https://codespaces.new/samlinux/appDev-motoko'>
+  <img src='https://github.com/codespaces/badge.svg' alt='Open in GitHub Codespaces' style='max-width: 100%;'>
+</a>
+
+This is a starter template for a Motoko / Svelte 5 project on the Internet Computer. It is designed to use it in Github Codespaces, but you can also use it locally.
+
+To get started with this template, click the "Open in GitHub Codespaces" button on the top right of this page.
+
+A ready to use Codespace environment will be created for you, and you can start developing your Motoko / Svelte 5 project right away.
+
+## Works
+- locally
+- Playground
---
+++ src/routes/+page.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import { ic } from '../stores/ic';
+
+  let greeting = $state('');
+  let input =  $state('');
+  let loading =  $state(false);
+
+  /**
+   * Handle the form submission
+   * @param event
+   */
+  async function handleOnSubmit(event: Event) {
+    loading = true;
+		greeting = 'loading ...';
+		try {
+			// Call the IC
+			greeting = await $ic.actor.sayHelloTo(input);
+			loading = false;
+
+		} catch (err: unknown) {
+			console.error(err);
+		}
+  }
+</script>
+
+<div class="p-4">
+  <h1 class="text-3xl font-bold underline p-2">
+    Hello freshman !
+  </h1>  
+  <div class="w-[50%]">
+    <p class="p-2">
+      Welcome to the Application Development Skills Course! This beginner-friendly course is designed to guide you through building web applications using the Motoko programming language on the Internet Computer. Whether you’re new to programming or seeking to expand your development skills, this course will provide the foundation you need to get started.
+    </p>
+    <p class="p-2">
+      This template is built with Svelte 5, SvelteKit, Tailwind CSS, and Motoko, offering a streamlined setup for developing web applications on the Internet Computer.
+    </p>
+  </div>
+
+  <div class="p-2">
+    <p>
+      Give me your name and say
+    </p>
+    <form onsubmit={handleOnSubmit}>
+			<input id="name" alt="Name" type="text" bind:value={input} placeholder="Say hello to"/>
+			<button type="submit" disabled={loading} >Click Me!</button>
+		</form>
+
+		<div> {greeting} </div>
+
+  </div>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
